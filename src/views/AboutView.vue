817f3e7c79a400a7f8882420d5b1aef5b17b6e7e
snail_88@126.com <snail_88@126.com>
2023-10-31 01:54:11 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="monacoEditor">
+    <Monaco :option="editorp" />
+  </div>
+</template>
+<script setup>
+import Monaco from "./MonacoEditor.vue";
+// 引入vue模块
+import { ref, reactive } from 'vue';
+const options = {
+  language: 'javascript',
+  code: `const str = "hello";`
+}
+
+const editorp = reactive({
+  el: 'monaco',
+  options: options
+});
+</script>
+<style scoped>
+.monacoEditor {
+  width: 900px;
+  height: 800px;
+}
+</style>
